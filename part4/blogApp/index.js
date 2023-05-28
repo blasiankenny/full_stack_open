ca5cc0561c97d2f8ac176c7b635f81bf11430a45
Kenny <88187389+blasiankenny@users.noreply.github.com>
2023-05-27 21:26:56 -0700

    Update index.js

--- part4/blogApp/index.js
+++ part4/blogApp/index.js
@@ -23,7 +23,7 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
